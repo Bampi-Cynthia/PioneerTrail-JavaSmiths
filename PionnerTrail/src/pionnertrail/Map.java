ff674c9aa6532956db33163848a33d8ddd7c6dc7
Elías <Elas@192.168.1.21>
2018-05-18 16:15:14 -0400

    Map class by Elías

--- PionnerTrail/src/pionnertrail/Map.java
+++ PionnerTrail/src/pionnertrail/Map.java
@@ -7,10 +7,94 @@ package pionnertrail;
 
 import java.io.Serializable;
 import java.util.Objects;
+import java.awt.Point;
 /**
- *
  * @author Elías
  */
-public class Map {
+public class Map implements Serializable{
+    
+    private String description;
+    // coordinate take place of currentRow and currentColumn.
+    private Point coordinates;
+    private int rowCount;
+    private int columnCount;
+    
+    //Default constructor
+    public Map(){
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Point getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(Point coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public int getRowCount() {
+        return rowCount;
+    }
+
+    public void setRowCount(int rowCount) {
+        this.rowCount = rowCount;
+    }
+
+    public int getColumnCount() {
+        return columnCount;
+    }
+
+    public void setColumnCount(int columnCount) {
+        this.columnCount = columnCount;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + Objects.hashCode(this.description);
+        hash = 59 * hash + Objects.hashCode(this.coordinates);
+        hash = 59 * hash + this.rowCount;
+        hash = 59 * hash + this.columnCount;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Map other = (Map) obj;
+        if (this.rowCount != other.rowCount) {
+            return false;
+        }
+        if (this.columnCount != other.columnCount) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.coordinates, other.coordinates)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Map{" + "description=" + description + ", coordinates=" + coordinates + ", rowCount=" + rowCount + ", columnCount=" + columnCount + '}';
+    }
     
 }
