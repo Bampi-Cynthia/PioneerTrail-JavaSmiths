30e11517a8d06247605d811aa609a481a3a90a48
Elías <Elas@192.168.1.21>
2018-05-18 15:59:04 -0400

    Map class

---
+++ PionnerTrail/src/pionnertrail/Map.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pionnertrail;
+
+import java.io.Serializable;
+import java.util.Objects;
+/**
+ *
+ * @author Elías
+ */
+public class Map {
+    
+}
