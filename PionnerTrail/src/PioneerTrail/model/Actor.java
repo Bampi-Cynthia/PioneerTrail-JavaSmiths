5cc94f9b91ae350f45da6ce73f2d1e987098239f
Elías <Elas@192.168.1.21>
2018-06-15 00:07:53 -0400

    Actor class rebuilt as an enum class

--- PionnerTrail/src/PioneerTrail/model/Actor.java
+++ PionnerTrail/src/PioneerTrail/model/Actor.java
@@ -1,122 +1,51 @@
 
 package PioneerTrail.model;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Objects;
 
 /**
  * @author Isaias
  */
 
-//This class will be a normal Class by now
-public class Actor implements Serializable{
+public enum Actor implements Serializable{
 
-    private String name;
-    private int health;
-    private int hunger;
-    //relationships between classes
-    private ArrayList<Player> players = new ArrayList<Player>();
-    private Game game;
-    private Wagon wagon;
-    private Scene scene;
-    // default constructor
-    public Actor(){
-    }
-    // setter and getter
+    Father("Johnson", "The father of the family, and leader of the Wagon", 5, 0),
+    Mother("Ms. Johnson", "The mother of the family, and Queen of the Wagon", 5, 0),
+    John("John", "The firstborn of the Jhonson family, and his brother's hero", 5, 0),
+    Maria("Maria", "The second child of the Jhonson family, and princess of the Wagon", 5, 0),
+    Jimmy("Jimmy","The last child of the Johnson family, he likes to play", 5, 1);
 
-    public ArrayList<Player> getPlayers() {
-        return players;
-    }
+    private final String name;
+    private final String description;
+    private final int health;
+    private final int hunger;
 
-    public void setPlayers(ArrayList<Player> players) {
-        this.players = players;
+Actor(String name, String description, int health, int hunger){
+this.name = name;
+this.description = description;
+this.health = health;
+this.hunger = hunger;
     }
 
-    public Game getGame() {
-        return game;
-    }
-
-    public void setGame(Game game) {
-        this.game = game;
-    }
-
-    public Wagon getWagon() {
-        return wagon;
-    }
-
-    public void setWagon(Wagon wagon) {
-        this.wagon = wagon;
-    }
-
-    public Scene getScene() {
-        return scene;
-    }
-
-    public void setScene(Scene scene) {
-        this.scene = scene;
-    }
-    
-    public void setname(String name) {
-        this.name = name;
-    }
-    
     public String getName() {
         return name;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     public int getHealth() {
         return health;
     }
 
-    public void setHealth(int health) {
-        this.health = health;
-    }
-
     public int getHunger() {
         return hunger;
     }
 
-    public void setHunger(int hunger) {
-        this.hunger = hunger;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 29 * hash + Objects.hashCode(this.name);
-        hash = 29 * hash + this.health;
-        hash = 29 * hash + this.hunger;
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Actor other = (Actor) obj;
-        if (this.health != other.health) {
-            return false;
-        }
-        if (this.hunger != other.hunger) {
-            return false;
-        }
-        if (!Objects.equals(this.name, other.name)) {
-            return false;
-        }
-        return true;
-    }
-
     @Override
     public String toString() {
-        return "Actor{" + "name=" + name + ", health=" + health + ", hunger=" + hunger + '}';
+        return "Actor{" + "name=" + name + ", description=" + description + ", health=" + health + ", hunger=" + hunger + '}';
     }
-    
+
 }
     
