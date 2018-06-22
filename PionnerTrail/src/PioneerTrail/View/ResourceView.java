ebd54efda636e7d2673dccba4c838b1edf76dfa5
Elías <Elas@192.168.1.21>
2018-06-22 00:44:36 -0400

    minor things in the ResourceView class

--- PionnerTrail/src/PioneerTrail/View/ResourceView.java
+++ PionnerTrail/src/PioneerTrail/View/ResourceView.java
@@ -1,7 +1,6 @@
 
 package PioneerTrail.View;
 
-import PioneerTrail.control.ResourceControl;
 import PioneerTrail.model.Resource;
 import java.util.ArrayList;
 import pionnertrail.PioneerTrail;
@@ -19,46 +18,6 @@ public class ResourceView extends View{
             + "Please, make your choice");
     }
     
-//    public void display() {
-//        boolean endOfView = false;
-//        String[] inputs;
-//        do {
-//            inputs = this.getInputs();
-//            if (inputs[0] == null || inputs[0].length() < 1 || inputs[0].toUpperCase().equals("Q")) {
-//                return;
-//            }
-//
-//            endOfView = doAction(inputs);
-//        } while (endOfView != true);
-//
-//    }
-//    
-//       private String[] getInputs() {
-//        Scanner scan;
-//        scan = new Scanner(System.in);
-//
-//        String[] inputs = new String[1];
-//        System.out.println(
-//                "===============================\n"
-//                + "            Resource Menu          \n"
-//                + "===============================\n"
-//                + "D - Display Resource List\n"    
-//                + "Q - Quit");
-//
-//        boolean valid = false;
-//        while (valid == false) {
-//            System.out.println("Please, enter your choice");
-//            inputs[0] = scan.nextLine();
-//            inputs[0] = inputs[0].trim();
-//            if (inputs[0].length() < 1) {
-//                System.out.println("You must enter a non-blank value");
-//                continue;
-//            }
-//            valid = true;
-//        }
-//        return inputs;
-//
-//    }
     @Override
     public boolean doAction(String inputs) {
 
@@ -81,10 +40,6 @@ public class ResourceView extends View{
      returnString += resources.get(i).toString() + "\n";
      }
      return returnString;
-//Resource[] list = sendList();
     }
-//    private void buildMenu(){
-//    return;
-//    }
 
 }
