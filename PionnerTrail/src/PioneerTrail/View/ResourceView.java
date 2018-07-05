1ef264b08683f00605b66d01e4a441c1ac34df83
Elías <Elas@192.168.1.21>
2018-07-04 22:43:26 -0400

    Resources menu changed to show an interactive list of items.

--- PionnerTrail/src/PioneerTrail/View/ResourceView.java
+++ PionnerTrail/src/PioneerTrail/View/ResourceView.java
@@ -1,45 +1,54 @@
-
 package PioneerTrail.View;
 
 import PioneerTrail.model.Resource;
 import java.util.ArrayList;
 import pionnertrail.PioneerTrail;
+
 /**
  * @author Cynthia
  */
-public class ResourceView extends View{
-    
+public class ResourceView extends View {
+
     public ResourceView() {
-        super("===============================\n"
-            + "            Resource Menu      \n"
-            + "===============================\n"
-            + "D - Display Resource List\n"    
-            + "Q - Quit \n"
-            + "Please, make your choice");
+//        super("===============================\n"
+//            + "            Resource Menu      \n"
+//            + "===============================\n"
+//            + "D - Display Resource List\n"    
+//            + "Q - Quit \n"
+//            + "Please, make your choice");
+        promptMessage = getResourceListString();
     }
-    
+
     @Override
     public boolean doAction(String inputs) {
-
-        String menuItem = inputs.toUpperCase();
-        switch (menuItem) {
-            case "D":
-               System.out.println(getResourceListString());
-                break;
-            default:
-                System.out.println("Invalid map menu item");
-                break;
+        ArrayList<Resource> resources = PioneerTrail.getCurrentGame().getResources();
+        int selection = Integer.parseInt(inputs);
+        if (selection < 0 || selection >= resources.size()) {
+            System.out.println("Invalid choice");
+            return false;
         }
+        
+        System.out.println("Name: " + resources.get(selection).getName());
+        System.out.println("Type: " + resources.get(selection).getInventoryType());
+        System.out.println("Quantity: " + resources.get(selection).getQuantity());
+        System.out.println("Weight: " + resources.get(selection).getWeight());
+
         return false;
     }
 
     private String getResourceListString() {
-     String returnString = "";
-     ArrayList<Resource> resources = PioneerTrail.getCurrentGame().getResources();
-     for(int i = 0; i < resources.size(); i++){
-     returnString += resources.get(i).toString() + "\n";
-     }
-     return returnString;
+        String returnString = "";
+        int maxWeight = 0;
+        ArrayList<Resource> resources = PioneerTrail.getCurrentGame().getResources();
+        for (int i = 0; i < resources.size(); i++) {
+            returnString += "\n" + i + " - " + resources.get(i).getName();
+            maxWeight = Integer.max(maxWeight, resources.get(i).getWeight());
+
+//             resources.get(i).toString() + "\n";
+        }
+        returnString += "\n The heaviest Item weighs:" + maxWeight;
+        returnString += "\n\n Select an Item (E to Exit)";
+        return returnString;
     }
 
 }
