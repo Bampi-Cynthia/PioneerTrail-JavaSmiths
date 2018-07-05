9b826350fc1c416f9d763a019cf0f3164891b3c5
Elías <Elas@192.168.1.21>
2018-07-05 01:31:22 -0400

    wood, ammo and shoes were added

--- PionnerTrail/src/PioneerTrail/model/ItemType.java
+++ PionnerTrail/src/PioneerTrail/model/ItemType.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package PioneerTrail.model;
 
 /**
@@ -13,5 +9,8 @@ public enum ItemType {
     food,
     water,
     tools,
-    parts
+    parts,
+    wood,
+    ammo,
+    shoes
 }
