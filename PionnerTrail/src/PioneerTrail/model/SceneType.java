f3ee1719fcde99770faca96bc843f2d3fd7a1984
Elías <Elas@192.168.1.21>
2018-07-06 12:20:04 -0400

    little fix

---
+++ PionnerTrail/src/PioneerTrail/model/SceneType.java
@@ -0,0 +1,38 @@
+
+package PioneerTrail.model;
+
+/**
+ * @author Elías
+ */
+public enum SceneType {
+    
+Nauvoo,
+SugarCreek,
+RichardsonsPoint,
+CharitonRiverCrossing,
+LocustCreek,
+
+GardenGrove,
+NishnabotnaRiver,
+GrandEncampment,
+CouncilBluffs,
+WinterQuarters,
+
+ElkhornRiverCrossing,
+PlatteRiver,
+FortKearny,
+ConfluencePoint,
+AshHollow,
+
+ChimneyRock,
+ScottsBluff,
+FortLaramie,
+SweetwaterRiver,
+IndependenceRock,
+
+FortBridger,
+EchoCanyon,
+GoldenPassRoad,
+EmigrationCanyon,
+Zion
+}
